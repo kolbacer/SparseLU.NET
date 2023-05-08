c0ae24d1758fcaa14882c597eab4e6c522bd3ae0
kolbacer <mitjas01@yandex.ru>
2023-05-08 10:18:00 +0300

    add benchmarks

---
+++ tests/SparseMatrixAlgebra.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+using SparseMatrixAlgebra.Benchmarks.Factorization;
+
+namespace SparseMatrixAlgebra.Benchmarks;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<CsrFactorizationBenchmark>();
+    }
+}
